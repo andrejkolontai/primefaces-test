0cefbbc93916e0565ac5ba77517b57bec1412594
Andrej Kolontai <kolontai@lmu.de>
2022-11-26 16:43:18 +0100

    Demo of ResourceUtils not recognizing resource URLs on Jakarta EE

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -2,8 +2,13 @@ package org.primefaces.test;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+
+import org.primefaces.util.ResourceUtils;
+
 import java.math.BigDecimal;
 import jakarta.annotation.PostConstruct;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.convert.FacesConverter;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Named;
 
@@ -13,6 +18,8 @@ import lombok.Data;
 @Named
 @ViewScoped
 public class TestView implements Serializable {
+	
+	private static final String JAKARTA_RESOURCE_URL = "/admin-ui/jakarta.faces.resource/9752-66110-07_3.jsf?ln=requestPhotosEdit";
     
     private String string;
     private Integer integer;
@@ -23,5 +30,22 @@ public class TestView implements Serializable {
     public void init() {
         string = "Welcome to PrimeFaces!!!";
     }
+    
+    public String getJakartaResourceURL() {
+    	return JAKARTA_RESOURCE_URL;
+    }
+    
+    public String getJavaxResourceURL() {
+    	return JAKARTA_RESOURCE_URL.replaceAll("jakarta\\.", "javax.");
+    }
+    
+    public String getJakartaFromResourceUtils() {
+    	FacesContext ctx = FacesContext.getCurrentInstance();
+    	return ResourceUtils.getResourceURL(ctx, this.getJakartaResourceURL());
+    }
 
+    public String getJavaxFromResourceUtils() {
+    	FacesContext ctx = FacesContext.getCurrentInstance();
+    	return ResourceUtils.getResourceURL(ctx, this.getJavaxResourceURL());
+    }
 }
